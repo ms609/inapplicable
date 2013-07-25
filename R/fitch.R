7dabbbd83367c93d7379f13bb6db3e963b98db6b
Martin R. Smith <ms609@users.noreply.github.com>
2013-07-25 10:21:53 +0000

    *** REMOVE .noC options.

--- R/fitch.R
+++ R/fitch.R
@@ -1,47 +1,5 @@
 parsimony.inapp <- function (tree, data) return (fitch.inapp(tree, data)[[1]])
 
-fitch.inapp.noC <- function (tree, data) {
-  # Data
-  if (class(data) == 'phyDat') data <- prepare.data.noC(data)
-  if (class(data) != '*phyDat') stop('Invalid data type; try data <- prepare.data(valid.phyDat.object).')
-  weight <- attr(data, 'weight')
-  nChar <- attr(data, 'nr') # strictly, transformation series patterns; these'll be upweighted later
-  # Tree
-  node <- tree$edge[,1]
-  edge <- tree$edge[,2]
-  tip.label <- tree$tip.label
-  nNode <- tree$Nnode
-  nTip <- nNode + 1
-  children <- sapply(1:nNode, function (x) {edge[node==nTip+x]})
-  root.node.index <- min(node)
-  # Workspace: matrix of possible states
-  na.matrix <- matrix(NA, ncol=nChar, nrow=32)
-  charlist <<- mclapply(1:(length(tip.label)+nTip-1), function (tl) {if (tl <= nTip) data[[tip.label[tl]]] else na.matrix} )
-  local.pscore <<- rep(0, nChar)
-  nLevel <- length(attr(data, 'levels'))
-  left.child   <- c(rep(0,nTip), children[1,])
-  right.child  <- c(rep(0,nTip), children[2,])
-  # Downpass function
-  fitch.downpass <- function (oNode) {
-    oLeft <- left.child[oNode]
-    oRight <- right.child[oNode]
-    left.state <- charlist[[oLeft]]
-    right.state <- charlist[[oRight]]
-    if (is.na(left.state[1])) left.state <- fitch.downpass(oLeft)
-    if (is.na(right.state[1])) right.state <- fitch.downpass(oRight)
-    fitch.result <- fitch.combine(left.state, right.state, nLevel)
-    
-    extra.pscore <- fitch.result[[2]]
-    local.pscore <<- local.pscore + extra.pscore
-    
-    charlist[[oNode]] <<- fitch.result[[1]]
-    return(fitch.result[[1]])
-  }
-  fitch.downpass(root.node.index)
-  pvec <- weight * local.pscore
-  return (list(sum(pvec), pvec))
-}
-
 fitch.inapp <- function (tree, data) {
   # Data
   if (class(data) == 'phyDat') data <- prepare.data(data)

25492a64a8275ce10f0ca41b70180fe5ea71ddc9
ms609 <ms609@b3b4ab76-6c86-4fdc-81e5-95d5aea05f56>
2013-07-25 10:21:53 +0000

    *** REMOVE .noC options.

--- inapplicable/R/prepare_data.R
+++ inapplicable/R/prepare_data.R
@@ -1,19 +1,3 @@
-prepare.data.noC <- function (data) {
-  at <- attributes(data)
-  cont <- at$contrast
-  nLevel <- length(at$level)
-  nTip <- length(data)
-  nChar <- attr(data, 'nr')
-  ret <- mclapply(data, function (x) {colSums(2^(0:(nLevel-1))*t(cont[x,]))})
-  ret <- mclapply(seq_along(data), function(x) { # for each node
-    if (x <= nTip) matrix(as.logical(intToBits(ret[[x]])), nrow=32)[-((nLevel+1):32),]
-  })
-  attributes(ret) <- at
-  names(ret) <- names(data)
-  class(ret) <- '*phyDat'
-  ret
-}
-
 prepare.data <- function (data) {
 # Written with reference to phangorn:::prepareDataFitch
   at <- attributes(data)

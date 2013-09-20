4ebe8af27496fea1cbe9c6ec4cbb10174b19b9f3
ms609 <ms609@b3b4ab76-6c86-4fdc-81e5-95d5aea05f56>
2013-09-20 16:08:48 +0000

    Propset; update Alfortithm III schematic (i.e. flow diagram) to reflect changes to downpass.  (Uppass coming up!)

--- inapplicable/src/fitch.c
+++ inapplicable/src/fitch.c
@@ -1,10 +1,7 @@
 #define USE_RINTERNALS
-
-
 #include <Rmath.h>
 #include <math.h>
 #include <R.h> 
-
 #include <Rinternals.h>
 
 void fitchdown(int *dat1, int *dat2, int *n_rows, int *pars, double *weight, int *inapp, double *w) {
@@ -27,7 +24,7 @@ void fitchdown(int *dat1, int *dat2, int *n_rows, int *pars, double *weight, int
           tmp = tmp - (*inapp);
         }
         (pars[k])++;
-        (*w) += weight[k];
+        (*w) += weight[k];  // TODO: does this need to be in a temporary w_tmp, only added to *w when we're sure we won't be fed a 'continue'?
       }
     }
     dat1[k] = tmp;
@@ -76,45 +73,51 @@ SEXP FITCHI(SEXP dat, SEXP nrx, SEXP node, SEXP edge, SEXP l, SEXP weight, SEXP 
   return(RESULT); 
 }
 
-void fitch_uppass(int *this_start, int *this_finish, int *ancestor_finish, int *childq_start, int *childr_start, int *n_rows, int *pars, double *weight, int *inapp, double *w) {
+void fitch_uppass(int *this_finish, int *this_start, int *ancestor_finish, int *childq_start, int *childr_start, int *n_rows, int *pars, double *weight, int *inapp, double *w) {
   int k, tmp_finish;
   for (k = 0; k < (*n_rows); k++) {
-    tmp_finish = dat1[k] & dat2[k]; // Intersect
+    tmp_finish = this_start[k] & ancestor_finish[k]; // Intersect
     if (tmp_finish != ancestor_finish[k]) {
-      if (childq_start & childr_start) {
-        tmp_finish <- ((childq_start | childr_start) & ancestor_finish) | this_start
-      } else {
-        tmp_finish <- this_start | ancestor_finish // Union
+      if (childq_start[k] & childr_start[k]) { // this_start was formed by intersecting childq and childr
+        tmp_finish = (((childq_start[k] | childr_start[k]) & ancestor_finish[k]) | this_start[k]);
+        pars[k] += 1;
+      } else { // this_start was formed by union of childq and childr
+        tmp_finish = (this_start[k] | ancestor_finish[k]); // Union
+        pars[k] += 1000000;
       }
-    }
+    } else pars[k] += 1000;
     this_finish[k] = tmp_finish;
+  }
 }
 
-void fitchupbridge(int *start_state, int *n_rows, int *pars, int *parent_of, int *child_of, int *n_node, double *weight, int *inapp, double *pvec, double *pscore) {
-  int i, prev_parent, k;
-  prev_parent = 0;
-  &finsh_state[(parent_of[0]-1L * (*n_rows)] = &start_state[(parent_of[0]-1L * (*n_rows)]; //Node 0, i.e. root
-  for (i = 1; i < *n_node; i++) { 
+void fitchupbridge(int *finish_state, int *start_state, int *n_rows, int *pars, int *parent_of, int *child_of, int *n_node, double *weight, int *inapp, double *pvec, double *pscore) {
+  int i;
+  // parent_of's first member is a child of the root node.  Thus parent_of[0] = root.number
+  for (i = 0; i < (*n_node)-1L; i++) {
     // parent_of is stored as 1L, 1R, 2L, 2R, 3L, 3R, 4L, 4R, ... nL, nR.  (The root node has no parent.)
     // child_of  is stored as 0L, 0R, 1L, 1R, 2L, 2R, 3L, 3R, 4L, 4R, ... nL, nR
     // start_state and finish_state are stored as [0 * n_rows] = states[,1], [1 * n_rows] = states[,2], ....
-    fitch_uppass(&start_state[(*n_node + i + 2L) * (*n_rows)], &finish_state[(*n_node + i + 2L) * (*n_rows)], &start_state[(parent_of[i-1L]-1L) * (*n_rows)], &start_state[(child_of[2L*i-1L]-1L) * (*n_rows)], &start_state[(child_of[2L*i]-1L) * (*n_rows)], nr, pars, weight, inapp, &pvec[(prev_parent-1L)]);
-    /*
-    if (prev_parent == parent[i]) {
-      // pvec[prev_parent-1L] += pvec[child[i]-1L]; // work out what this is doing later
-      fitchuppass(&dat[(prev_parent-1L) * (*n_rows)], &dat[(child[i]-1L) * (*n_rows)], nr, pars, weight, inapp, &pvec[(prev_parent-1L)]);
-    } else {
-      prev_parent = parent[i];
-      // pvec[(prev_parent-1L)] += pvec[(child[i]-1L)]; // work out what this is doing later
-      // for(k = 0; k < (*n_rows); k++) dat[(prev_parent-1L)*(*n_rows) + k] = dat[(child[i]-1L)*(*n_rows) + k]; // work out what this is doing later
-    }
-  */
+    // Worked examples assume that root node = 11 and i = 0, meaning 'look at node 12' [the first of 11's children].
+    fitch_uppass(
+    //  The position of node 12 in the finish_state and start_state array is:
+    //    root.number (counting from 1) + i = i.e. position[11], the 12th position
+    &finish_state[(parent_of[0] + i) * (*n_rows)], &start_state[(parent_of[0] + i) * (*n_rows)], // this_finish, this_start
+    //  To find the number of node 12's parent we look in parent_of[node12.index]
+    //    parent_of[0] is the parent of node [root + i] = 12th node
+    //    node12.index = i = 0; parent_of[0] = 11; so we need start_state[11-1]
+    &finish_state[(parent_of[i]-1L) * (*n_rows)], // ancestor_finish
+    //  To find the number of node 12's children we look in child_of[node12.index]
+    //    child_of[0, 1] are the two children of node [root + i] = 12
+    //    node12.index = i = 0; child_of[0*2] = Q; child_of[0*2 + 1] = R
+    &start_state[(child_of[i * 2L]-1L) * (*n_rows)], &start_state[(child_of[(i * 2L) + 1L]-1L) * (*n_rows)], // childq_start, childr_start
+    n_rows, pars, weight, inapp, &pvec[i]);
+    // # TODO : Worry about pvec
   }
-  pscore[0]=pvec[ni-1];
+  // pscore[0]=pvec[ni-1];
 }
 
-SEXP FITCHUP(SEXP dat, SEXP n_transform_series, SEXP parent_of, SEXP child_of, SEXP n_node, SEXP weight, SEXP max_node, SEXP n_tip, SEXP inapp) {   
-  int *data, *n_rows=INTEGER(n_transform_series), m=INTEGER(max_node)[0], i, n=INTEGER(n_tip)[0];   
+SEXP FITCHUP(SEXP dat, SEXP n_transform_series, SEXP parent_of, SEXP child_of, SEXP n_node, SEXP weight, SEXP max_node, SEXP n_tip, SEXP inapp) {
+  int *start_state, *finish_state, *n_rows=INTEGER(n_transform_series), m=INTEGER(max_node)[0], i, n=INTEGER(n_tip)[0];   
   double *pvtmp;
   SEXP DAT, pars, pvec, pscore, RESULT;
   PROTECT(RESULT = allocVector(VECSXP, 4L));
@@ -123,14 +126,14 @@ SEXP FITCHUP(SEXP dat, SEXP n_transform_series, SEXP parent_of, SEXP child_of, S
   PROTECT(DAT = allocMatrix(INTSXP, n_rows[0], m));
   PROTECT(pvec = allocVector(REALSXP, m));
   pvtmp = REAL(pvec);
-  data = INTEGER(DAT);
-  finish_state = INTEGER(DAT);
+  start_state = INTEGER(DAT);
   for(i=0; i<m; i++) pvtmp[i] = 0.0;
   for(i=0; i<*n_rows; i++) INTEGER(pars)[i] = 0L;
   REAL(pscore)[0]=0.0;
-  for(i=0; i<(*n_rows * n); i++) data[i] = INTEGER(dat)[i];
+  for(i=0; i<(*n_rows * n); i++) start_state[i] = INTEGER(dat)[i];
+  finish_state = start_state;
   
-  fitchupbridge(data, finish_state, n_rows, INTEGER(pars), INTEGER(parent_of), INTEGER(child_of), INTEGER(n_node), REAL(weight), INTEGER(inapp), pvtmp, REAL(pscore));
+  fitchupbridge(start_state, finish_state, n_rows, INTEGER(pars), INTEGER(parent_of), INTEGER(child_of), INTEGER(n_node), REAL(weight), INTEGER(inapp), pvtmp, REAL(pscore));
   
   SET_VECTOR_ELT(RESULT, 0, pscore);
   SET_VECTOR_ELT(RESULT, 1, pars);

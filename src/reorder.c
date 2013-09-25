3404a501ed9d7b9565f0c8e8c2973bf5c5a64754
Martin R. Smith <ms609@users.noreply.github.com>
2013-09-25 12:38:15 +0000

    spr in nni time!

--- src/reorder.c
+++ src/reorder.c
@@ -19,7 +19,7 @@ void order_edges(int *parent, int *child, int *n_node, int *n_edge) {
     start_c[i] = child[i];
     q_pos = parent[i] - root_node;
     if (child_l[q_pos]) {
-      if (child_l[q_pos] < child[i]) {
+      if (child_l[q_pos] < child[i] && child[i] > root_node) {
         child_r[q_pos] = child[i];
       } else {
         child_r[q_pos] = child_l[q_pos];
@@ -43,5 +43,19 @@ void order_edges(int *parent, int *child, int *n_node, int *n_edge) {
       queue_c[q_pos++] = child_r[o_node - root_node];
       o_node = child_l[o_node - root_node];
     }
+  }
+}
+
+void number_nodes(int *parent, int *child, int *root_node, int *n_edge) {
+  int i, next_node, n_allnodes = *n_edge + 1L;
+  int * renumber = calloc(n_allnodes, sizeof(int));
+  next_node = *root_node;
+  for (i = 0; i < n_allnodes; i++) renumber[i] = i + 1L;
+  for (i = 0; i < *n_edge; i++) {
+    if (child[i] > *root_node) renumber[child[i]-1L] = ++(next_node);
+  }
+  for (i = 0; i < *n_edge; i++) {
+    parent[i] = renumber[parent[i]-1L];
+    child[i] = renumber[child[i]-1L];
   }
 }

36c75cc53b40438aba65b9138fd45bbb5c822d05
Martin R. Smith <ms609@users.noreply.github.com>
2013-11-06 14:58:05 +0000

    Fix algorithm, apparently, and improve vis

--- R/visualise.R
+++ R/visualise.R
@@ -10,39 +10,38 @@ visualize.character <- visualise.character <- visualize.char <- visualise.char <
   child <- tree.edge[,2]
   tip.label <- tree$tip.label
   nEdge <- length(parent)
-  maxNode <- parent[1] #max(parent)
   nTip <- length(tip.label)
-  nNode <- maxNode - nTip
+  nNode <- nTip - 1
+  maxNode <- nNode + nTip
   inapp <- at$inapp.level
-  allNodes <- (nTip + 1L):maxNode
-  parentof <- parent[match((nTip + 2L):maxNode, child )]
-  childof <- child [c(match(allNodes, parent), length(parent) + 1L - match(allNodes, rev(parent)))]
+  tips <- seq(nTip)
+  nodes <- nTip + seq(nNode)
+  parentof <- parent[match((nTip + 2L):maxNode, child )] # Exclude the root, which has no parent
+  childof <- child [c(match(nodes, parent), length(parent) + 1L - match(nodes, rev(parent)))]
   
   down <- .Call("FITCHDOWN", char.dat[tip.label], as.integer(1), as.integer(parent), as.integer(child), as.integer(nEdge), as.double(1), as.integer(maxNode), as.integer(nTip), as.integer(inapp), PACKAGE='inapplicable') # Return: (1), pscore; (2), pars; (3), DAT; (4), pvec; (5), need_up
   up <- .Call("FITCHUP", as.integer(down[[3]]), as.integer(1), as.integer(parentof), as.integer(childof), as.integer(nNode), as.double(1), as.integer(maxNode), as.integer(nTip), as.integer(inapp), PACKAGE='inapplicable')
   
   plot.fun(tree)
+  text(1,1,paste0('downpass +', down[[2]], '; uppass +', up[[2]], '; total +', down[[2]] + up[[2]]), pos=4, cex=0.8)
   
   downpass.states <- down[[3]]
   down.scorers <- down[[4]]
-  down.no.change <- non.scorer(parent, child, nTip, down.scorers)
-  tiplabels(possible.tokens(at$levels, downpass.states[1,seq_along(tree$tip.label)]), adj=c(0.3,0.5), bg='white', col='#000088', cex=0.75)
-  nodelabels(possible.tokens(at$levels, downpass.states[1,nTip + seq(tree$Nnode)]), adj=rep(1.25,2), frame='no', bg='white', font=ifelse(down.no.change, 1,2) , col=ifelse(down.no.change, '#880000cc', '#cc3333'), cex=ifelse(down.no.change, 0.6,1))
+  down.change <- sapply(nodes, function(n) {
+      children <- child[parent==n]
+      return (down.scorers[n] != down.scorers[children[1]] + down.scorers[children[2]])   
+    })
+  #nodelabels(down.scorers[nodes])
+  tipcols = c('#fafafa', '#fafafa', '#ffbbbb88', '#bbffbb', '#bbbbff', '#bbbbff', '#bbffbb', '#ffbbbb', '#bbddff', '#ffbbdd')
+  names(tipcols) <- c(NA, max(downpass.states[1,]), 2^(0:7))
+  tipcols[as.character(inapp)] <- '#999999'
+  tiplabels(possible.tokens(at$levels, downpass.states[1,tips]), adj=c(0.3,0.5), bg=tipcols[as.character(downpass.states[1,tips])], col='#000088', cex=0.85)
+  nodelabels(possible.tokens(at$levels, downpass.states[1,nodes]), adj=rep(1.25,2), frame='no', bg=ifelse(down.change, 'white', 'yellow'), font=ifelse(down.change, 2, 1) , col=ifelse(down.change, '#cc3333', '#880000cc'), cex=ifelse(down.change,1,0.6))
   
   uppass.states <- up[[3]]
-  uppass.scorers <- up[[4]]
-  up.no.change <- non.scorer(parent, child, nTip, uppass.scorers)  
-  nodelabels(possible.tokens(at$levels, uppass.states[1,nTip + seq(tree$Nnode)]), adj=c(1.25,-0.75), frame='no', bg='white', font=ifelse(up.no.change, 1,2) , col=ifelse(up.no.change, '#008800cc', '#008800'), cex=ifelse(up.no.change, 0.6,1))
-  text(1,1,paste0('downpass +', down[[2]], '; uppass +', up[[2]], '; total +', down[[2]] + up[[2]]), pos=4, cex=0.8)
-}
-
-non.scorer <- function (parent, child, nTip, states) {
-  tips <- 1:nTip
-  nodes <- (nTip + 1):length(states)
-  sapply(nodes, function(n) {
-    children <- child[parent==n]
-    return (states[n] == states[children[1]] ||states[n] == states[children[2]])   
-  })
+  up.scorers <- up[[4]]
+  up.change <- as.logical(up.scorers[nodes])
+  nodelabels(possible.tokens(at$levels, uppass.states[1,nodes]), adj=c(1.25,-0.75), frame='no', bg='white', font=ifelse(up.change, 2, 1), col=ifelse(up.change, '#008800', '#008800cc'), cex=ifelse(up.change, 1,0.6))
 }
 
 possible.tokens <- function (lvls, number) {

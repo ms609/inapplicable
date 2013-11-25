f8e2cfc1fbddb144372a0936dd856fe526b0599f
ms609 <ms609@b3b4ab76-6c86-4fdc-81e5-95d5aea05f56>
2013-11-25 11:03:54 +0000

    Use -1 throughout appl, to fix bugs in fitch.C

--- inapplicable/src/fitch.c
+++ inapplicable/src/fitch.c
@@ -292,192 +292,42 @@ SEXP FITCHDOWN(SEXP dat, SEXP nrx, SEXP parent, SEXP child, SEXP n_edge, SEXP we
   return(RESULT);
 }
 
-void fitch_uproot(int *this, int *child_q, int *child_r, int *n_rows, int *pars, double *weight, int *inapp, double *w) {
-  int k, ancestor_k, tmp, applicables = ~*inapp;
-  for (k = 0; k < (*n_rows); k++) { // Next TS
-    ancestor_k = (this[k] == *inapp) ? ~0 : this[k] & applicables;
-    
-    //// The below code corresponds to fitch_upnode, but with ancestor_k in place of ancestor[k]
-    
-    if (
-      (((ancestor_k & applicables) ? 1 : 0) + ((child_q[k] & applicables) ? 1 : 0) + ((child_r[k] & applicables) ? 1 : 0)) // number of relatives with applicable token
-      >= 
-      (((ancestor_k &  *inapp) ? 1 : 0) + ((child_q[k] &  *inapp) ? 1 : 0) + ((child_r[k] &  *inapp) ? 1 : 0)) // number of relatives with inapplicable token
-    ) {
-      if (child_q[k] & child_r[k] & *inapp) { // Children both have inapplicable token
-        if (((child_q[k] & applicables) && (child_r[k] & applicables)) // Children both have an applicable token
-          && !(child_q[k] & child_r[k] & applicables) // Children do not have applicable tokens in common
-        ) {
-        (pars[k])++; (*w) += weight[k];  // Increase parsimony score by one
-        } else if (((tmp = child_q[k] | child_r[k]) & applicables) // Either child has an applicable token
-            && (ancestor_k & applicables)) { // Parent has applicable token
-          if (ancestor_k & tmp & applicables) { // Parent has an applicable token in common with either child
-            this[k] = ((child_q[k] & ancestor_k) | (child_r[k] & ancestor_k)) &  applicables;
-          } else {
-            if (child_q[k] & child_r[k] & applicables) {
-              this[k] = tmp & applicables; // Set this node's tokens to applicable tokens in either child
-            } else {
-              this[k] = (tmp | ancestor_k) & applicables; // Set this node's tokens to applicable tokens in any adjacent node            
-              (pars[k])++; (*w) += weight[k];  // Increase parsimony score by one
-            }
-          }
-        }
-      }
-      // At ROOT node, the IF that is here in _upnode always evaluates to TRUE; thus:
-      this[k] = ancestor_k;
-    } else {
-      this[k] = *inapp;
-    }
-  }
-}
-
-void fitch_upnode(int *this, int *ancestor, int *child_q, int *child_r, int *n_rows, int *pars, double *weight, int *inapp, double *w) {
-  int k, tmp, applicables = ~*inapp;
-  for (k = 0; k < (*n_rows); k++) {    // Next TS
-    if (
-      (((ancestor[k] & applicables) ? 1 : 0) + ((child_q[k] & applicables) ? 1 : 0) + ((child_r[k] & applicables) ? 1 : 0)) // number of relatives with applicable token
-      >= 
-      (((ancestor[k] & *inapp) ? 1 : 0) + ((child_q[k] & *inapp) ? 1 : 0) + ((child_r[k] & *inapp) ? 1 : 0)) // number of relatives with inapplicable token
-    ) {
-      if (child_q[k] & child_r[k] & *inapp) { // Children both have inapplicable token
-        if (((child_q[k] & applicables) && (child_r[k] & applicables)) // Children both have an applicable token
-          && !(child_q[k] & child_r[k] & applicables) // Children do not have applicable tokens in common
-        ) {
-          (pars[k])++; (*w) += weight[k];  // Increase parsimony score by one
-        } else if (((tmp = child_q[k] | child_r[k]) & applicables) // Either child has an applicable token
-            && (ancestor[k] & applicables)) {           // Parent has applicable token
-          if (ancestor[k] & tmp & applicables) { // Parent has an applicable token in common with either child
-            this[k] = ((child_q[k] & ancestor[k]) | (child_r[k] & ancestor[k])) &  applicables;
-          } else {
-            if (child_q[k] & child_r[k] & applicables) {
-              this[k] = tmp & applicables; // Set this node's tokens to applicable tokens in either child
-            } else {
-              this[k] = (tmp | ancestor[k]) & applicables; // Set this node's tokens to applicable tokens in any adjacent node            
-              (pars[k])++; (*w) += weight[k];  // Increase parsimony score by one
-            }
-          }
-          continue;
-        }
-      }
-      // At ROOT node, the next IF will be true; accelerate final code by deleting conditional
-      if ((ancestor[k] != *inapp)
-      && (((ancestor[k] & this[k]) | *inapp) == (ancestor[k] | *inapp))) { // Ignoring {-}, parent node's tokens present in both child nodes
-        this[k] = ancestor[k] & applicables;
-      } else {
-        if ((tmp = child_q[k] & child_r[k]) && (tmp != *inapp)) { // Children have tokens in common, excluding {-}
-          this[k] |= (ancestor[k] & (child_q[k] | child_r[k])); // Set this node's tokens to the tokens present in the parent or either child
-        } else {
-          this[k] |= ancestor[k]; // Add parent's tokens to this node's tokens
-        }
-        this[k] &= applicables; // Remove {-} from this node's tokens
-      }
-    } else {
-      this[k] = *inapp;
-    }
-  }
-}
-
-void fitch_uppass(int *state, int *parent_of, int *children_of, int *n_rows, int *pars, int *n_node, double *weight, double *pvec, int *inapp, double *pscore) {
-  int i;
-  fitch_uproot(&state[(parent_of[0]-1) * (*n_rows)], // this_start, will become this_finish
-               &state[(children_of[0      ]-1) * (*n_rows)], // child q
-               &state[(children_of[*n_node]-1) * (*n_rows)], // child r
-    n_rows, pars, weight, inapp, &pvec[parent_of[0]-1]);
-  pscore[0] += pvec[parent_of[0] - 1];
-  // parent_of's first member is a child of the root node.  Thus parent_of[0] = root.number
-  for (i = 0; i < (*n_node)-1; i++) {
-    // parent_of is stored as 1, 1R, 2L, 2R, 3, 3R, 4L, 4R, ... nL, nR.  (The root node has no parent.)
-    // children_of is stored as 0, 1, 2L, ... nL, 0R, 1R, 2R, 3R, ..., nR
-    // state and state are stored as [0 * n_rows] = states[,1], [1 * n_rows] = states[,2], ....
-    
-    // Worked example assumes that root node = 11 and i = 0, meaning 'look at node 12' [the first of 11's children].
-    fitch_upnode(
-    //  The position of node 12 in the state array is:
-    //    root.number (counting from 1) + i = i.e. position[11], the 12th position
-    &state[(parent_of[0] +1-1 + i) * (*n_rows)], // this_start, will become this_finish
-    //  To find the number of node 12's parent we look in parent_of[node12.index]
-    //    parent_of[0] is the parent of node [root + i] = 12th node
-    //    node12.index = i = 0; parent_of[0] = 11; so we need state[11-1]
-    &state[(parent_of[i]-1) * (*n_rows)], // ancestor
-    //  To find the number of node 12's children we look in children_of[node12.index]
-    //    children_of[0, *n_node + 0] are the two children of node [root + i] = 12
-    //    node12.index = i = 0; children_of[0*2] = Q; children_of[0*2 + 1] = R
-    &state[(children_of[i + 1]-1) * (*n_rows)], // child q
-    &state[(children_of[i + 1 + *n_node]-1) * (*n_rows)], // child r
-    n_rows, pars, weight, inapp, &pvec[parent_of[0] +1-1 + i]);
-    pscore[0] += pvec[parent_of[0] +1-1 + i];
-  }
-}
-
-SEXP FITCHUP(SEXP dat, SEXP n_transform_series, SEXP parent_of, SEXP children_of, SEXP n_node, SEXP weight, SEXP max_node, SEXP n_tip, SEXP inapp) {
-  int *state, *n_rows=INTEGER(n_transform_series), m=INTEGER(max_node)[0], i;   
-  double *pvtmp;
-  SEXP DATA, pars, pscore, pvec, RESULT;
-  PROTECT(RESULT = allocVector(VECSXP, 4L));
-  PROTECT(pars = allocVector(INTSXP, *n_rows));
-  PROTECT(pscore = allocVector(REALSXP, 1));
-  PROTECT(pvec = allocVector(REALSXP, m));
-  PROTECT(DATA = allocMatrix(INTSXP, n_rows[0], m));
-  pvtmp = REAL(pvec);
-  for(i=0; i<m; i++) pvtmp[i] = 0.0;
-  for(i=0; i<*n_rows; i++) INTEGER(pars)[i] = 0;
-  REAL(pscore)[0] = 0.0;
-  state = INTEGER(DATA);
-  for(i=0; i<(*n_rows * m); i++) state[i] = INTEGER(dat)[i];
-  
-  fitch_uppass(state, INTEGER(parent_of), INTEGER(children_of), n_rows, INTEGER(pars), INTEGER(n_node), REAL(weight), pvtmp, INTEGER(inapp), REAL(pscore));
-  
-  SET_VECTOR_ELT(RESULT, 0, pscore);
-  SET_VECTOR_ELT(RESULT, 1, pars);
-  SET_VECTOR_ELT(RESULT, 2, DATA);
-  SET_VECTOR_ELT(RESULT, 3, pvec);
-  UNPROTECT(5);
-  return(RESULT); 
-}
-
-
-void fitch_app_downnode(int *app, int *dat1, int *dat2, int *n_rows, int *pars, double *weight, int *inapp, double *w) {
+void fitch_app_downnode(int *app, int *this, int *child_q, int *child_r, int *n_rows, int *pars, double *weight, int *inapp, double *w) {
   int k, tmp;
   for (k = 0; k < (*n_rows); k++) {
-    if (app[k] == 0) {
-      dat1[k] = *inapp;
-    } else if (dat1[k] == *inapp) {
-      dat1[k] = dat2[k] & ~*inapp;
-    } else if (dat2[k] == *inapp) {
-      dat1[k] &= ~*inapp;
-    } else if ((tmp = (dat1[k] & dat2[k] & ~*inapp))) { // Applicable tokens in Common
-      dat1[k] = tmp;
+    if (app[k] == -1) {
+      this[k] = *inapp;
+    } else if (child_q[k] == *inapp) {
+      this[k] = child_r[k] & ~*inapp;
+    } else if (child_r[k] == *inapp) {
+      this[k] = child_q[k] & ~*inapp;
+    } else if ((tmp = (child_q[k] & child_r[k] & ~*inapp))) { // Applicable tokens in Common
+      this[k] = tmp;
     } else {
-      dat1[k] = (dat1[k] | dat2[k]) & ~*inapp;
+      this[k] = (child_q[k] | child_r[k]) & ~*inapp;
       (pars[k])++; (*w) += weight[k]; // Increase parsimony score by one
     }
   }
 }
 
 void fitch_app_downpass(int *dat, int *app, int *n_rows, int *pars, int *parent, int *child, int *n_edge, double *weight, int *inapp, double *pvec, double *pscore) {
-  int i, ni, k;
-  ni = 0;
-  for (i = 0; i < *n_edge; i++) {
-    if (ni == parent[i]){
-      pvec[ni-1] += pvec[child[i]-1];
-      fitch_app_downnode(&app[(ni-1)*(*n_rows)], &dat[(ni-1) * (*n_rows)], &dat[(child[i]-1) * (*n_rows)], n_rows, pars, weight, inapp, &pvec[(ni-1)]);
-    } else {
-      ni = parent[i];
-      pvec[ni-1] += pvec[child[i]-1];        
-      for (k = 0; k < (*n_rows); k++) dat[(ni-1)*(*n_rows) + k] = dat[(child[i]-1)*(*n_rows) + k];                     
-    }
+  int i, parent_i;
+  for (i = 0; i < *n_edge; i+=2) {
+    parent_i = parent[i];
+    pvec[parent_i -1] += pvec[child[i] -1] + pvec[child[i+1] -1];
+    fitch_app_downnode(&app[(parent_i -1)*(*n_rows)], &dat[(parent_i -1)* (*n_rows)], &dat[(child[i]-1) * (*n_rows)], &dat[(child[i+1]-1) * (*n_rows)], n_rows, pars, weight, inapp, &pvec[(parent_i -1)]);
   }
-  pscore[0] = pvec[ni-1];
+  pscore[0] = pvec[parent_i-1];
 }
 
 void app_upnode(int *this, int *ancestor, int *child_q, int *child_r, int *n_rows) {
   int k; // Iterate through each transformation series
-  for (k = 0; k < (*n_rows); k++) this[k] = (((child_q[k] + child_r[k] + (ancestor[k] ? 1 : -1)) >= 0) ? 1 : 0);
+  for (k = 0; k < (*n_rows); k++) this[k] = ((child_q[k] + child_r[k] + ancestor[k] >= 0) ? 1 : -1);
 }
 
 void app_uproot(int *this, int *n_rows) {
   int k; // Iterate through each transformation series
-  for (k = 0; k < (*n_rows); k++) this[k] = ((this[k] > -1) ? 1 : 0); // Assume applicable if ambiguous.
+  for (k = 0; k < (*n_rows); k++) this[k] = ((this[k] > -1) ? 1 : -1); // Assume applicable if ambiguous.
 }
 
 void app_uppass(int *app, int *n_rows, int *parent_of, int *children_of, int *n_node) {
@@ -509,7 +359,9 @@ void app_uppass(int *app, int *n_rows, int *parent_of, int *children_of, int *n_
 
 void app_downnode(int *dat, int *child_q, int *child_r, int *n_rows) {
   int k, tmp;
-  for (k = 0; k < (*n_rows); k++) dat[k] = ((tmp = (child_q[k] + child_r[k])) ? (tmp > 0 ? 1 : -1) : 0);
+  for (k = 0; k < (*n_rows); k++) {
+    dat[k] = ((tmp = (child_q[k] + child_r[k])) ? (tmp > 0 ? 1 : -1) : 0);
+  }
 }
 
 void app_downpass(int *app, int *n_rows, int *parent, int *child, int *n_edge) {

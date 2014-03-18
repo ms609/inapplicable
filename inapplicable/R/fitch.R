4e4905c01d9f30e9785bc45337fb99b31ca8bdc5
ms609 <ms609@b3b4ab76-6c86-4fdc-81e5-95d5aea05f56>
2014-03-18 18:04:51 +0000

    Some integration with Information

--- inapplicable/R/fitch.R
+++ inapplicable/R/fitch.R
@@ -1,9 +1,9 @@
 parsimony.inapp <- function (tree, data, inherit.ancestral = FALSE, target = NULL, criterion=NULL) {
-  if (is.null(criterion)) return (fitch.inapp(tree, data, target=target, inherit.ancestral=inherit.ancestral)[[1]]) 
+  if (is.null(criterion)) return (fitch.inapp(tree, data)[[1]]) 
   return (tree.information(tree, data))
 }
 
-fitch.inapp <- function (tree, data, inherit.ancestral = FALSE, target = NULL) {
+fitch.inapp <- function (tree, data) {
   # Data
   if (class(data) == 'phyDat') data <- prepare.data(data)
   if (class(data) != '*phyDat') stop('Invalid data type; try fitch.inapp(tree, data <- prepare.data(valid.phyDat.object)).')
@@ -27,4 +27,40 @@ fitch.inapp <- function (tree, data, inherit.ancestral = FALSE, target = NULL) {
   ret <- .Call("FITCHINAPP", data[, tip.label], as.integer(nChar), as.integer(parent), as.integer(child), as.integer(parentof), as.integer(childof), as.integer(nEdge), as.integer(nNode), as.double(weight), as.integer(maxNode), as.integer(nTip), as.integer(inapp), PACKAGE='inapplicable') # 
   
   return (ret[1:3])
+}
+
+fitch.info <- function (tree, data) {
+  if (class(data) == 'phyDat') data <- prepare.data(data)
+  if (class(data) != '*phyDat') stop('Invalid data type; try fitch.inapp(tree, data <- prepare.data(valid.phyDat.object)).')
+  at <- attributes(data)
+  nChar  <- at$nr # strictly, transformation series patterns; these'll be upweighted later
+  weight <- at$weight
+  if (is.null(at$order) || at$order == "cladewise") tree <- reorder(tree, "postorder")
+  tree.edge <- tree$edge
+  parent <- tree.edge[,1]
+  child <- tree.edge[,2]
+  tip.label <- tree$tip.label
+  nEdge <- length(parent)
+  nTip <- length(tip.label)
+  nNode <- nTip - 1
+  maxNode <- nNode + nTip
+  inapp <- at$inapp.level
+  tips <- seq(nTip)
+  nodes <- nTip + seq(nNode)
+  parentof <- parent[match((nTip + 2L):maxNode, child )] # Exclude the root, which has no parent
+  allNodes <- (root <- nTip + 1L):maxNode
+  childEdge <- c(match(allNodes, parent), length(parent) + 1L - match(allNodes, rev(parent)))
+  childof <- child[childEdge]
+  if (any(is.na(data[, tip.label]))) stop("Tree's tip labels could not all be found in data matrix")
+  ret <- .Call("FITCHTRANS", data[, tip.label], as.integer(nChar), as.integer(parent), as.integer(child), as.integer(parentof), as.integer(childof),  as.integer(childEdge), as.integer(nEdge), as.integer(nNode), as.integer(maxNode), as.integer(nTip), as.integer(inapp)) # 
+  extras <- apply(ret[[4]], 1, function (x) {
+    unx <- unique(x)
+    unx <- unx[unx>0]
+    uniques <- vapply(unx, function (u) sum(x==u), integer(1))
+    sum(uniques - 1)
+  })
+  il <- vapply(1:nChar, function (i) {
+    information.loss(colSums(as.split(data[i, ], attr(data, 'inapp.level'))), extras[i])
+  }, double(1))
+  -sum(il)
 }

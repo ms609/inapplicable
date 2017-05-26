cb5e0df12aec885d8d6944abf2b8514c26f5a6a9
Martin R. Smith <martins@gmail.com>
2017-05-26 17:48:06 +0100

    Include root node in downpasses!

--- src/RMorphy.c
+++ src/RMorphy.c
@@ -19,7 +19,7 @@ SEXP MORPHYLENGTH(SEXP R_ancestors, SEXP R_left, SEXP R_right, SEXP MorphyHandl)
   const int max_node = n_taxa + n_internal - 1;
   
   // R_descendants and R_ancestors have already had one subtracted to convert them to an index 
-  const int *ancestor=INTEGER(R_ancestors), *left=INTEGER(R_descendants), 
+  const int *ancestor=INTEGER(R_ancestors), *left=INTEGER(R_left), 
             *right=INTEGER(R_right); // INTEGER gives pointer to first element of length n R vector
   
   // Declare and protect result, to return to R
@@ -31,20 +31,23 @@ SEXP MORPHYLENGTH(SEXP R_ancestors, SEXP R_left, SEXP R_right, SEXP MorphyHandl)
   pscore_temp = INTEGER(Rres);
   *pscore_temp = 0;
    
-  for (i = max_node - 1; i > n_taxa; i--) { // First Downpass 
+  for (i = max_node - 1; i >= n_taxa; i--) { // First Downpass 
     *pscore_temp += mpl_first_down_recon(i,  left[i - n_taxa], right[i - n_taxa], handl);
+    //Rprintf("Downpass on node %i -< %i,%i ... pscore is %i\n", i, left[i-n_taxa], right[i-n_taxa], *pscore_temp);
+    
   }
-  mpl_update_lower_root(max_node, root_node, handl); // The index of the lower dummy root node = max_node/
-                                                     // TODO we can alternatively pass the root node as its own ancestor.
+  mpl_update_lower_root(root_node, root_node, handl); // We could use a spare internal node with index = max_node as a dummy root node.
+                                                      // Or we can just pass the root node as its own ancestor.
  
   for (i = root_node; i < max_node; i++) { // First uppass: internal nodes
     *pscore_temp += mpl_first_up_recon(i, left[i - n_taxa], right[i - n_taxa], ancestor[i], handl);
+    //Rprintf("Uppass on node %i -< %i,%i ... pscore is %i\n", i, left[i-n_taxa], right[i-n_taxa], *pscore_temp);
   }
   for (i = 0; i < n_taxa; i++) { // First uppass: update tips
     mpl_update_tip(i, ancestor[i], handl);
   }
   
-  for (i = max_node - 1; i > n_taxa; i--) { // Second Downpass 
+  for (i = max_node - 1; i >= n_taxa; i--) { // Second Downpass 
     *pscore_temp += mpl_second_down_recon(i, left[i - n_taxa], right[i - n_taxa], handl);
   }
  

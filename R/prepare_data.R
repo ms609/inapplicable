f008d123deb686fd4d280bdb88dbb882132188c0
Martin R. Smith <martins@gmail.com>
2017-05-26 13:37:51 +0100

    Clarify formatting; change order of events

--- R/prepare_data.R
+++ R/prepare_data.R
@@ -131,18 +131,27 @@ LoadMorphy <- function (phy) {
   morphyObj <- mpl_new_Morphy()
   nTax <- length(phy)
   nChar <- length(phy[[1]])
-  if(mpl_init_Morphy(nTax, nChar, morphyObj) -> error) stop("Error ", mpl_translate_error(error), " in mpl_init_Morphy")
-  if(mpl_attach_rawdata(PhyToString(phy, ';'), morphyObj) -> error) stop("Error ", mpl_translate_error(error), " in mpl_attach_rawdata")
-  if(mpl_set_num_internal_nodes(nTax + 1L, morphyObj) -> error) stop("Error ", mpl_translate_error(error), " in mpl_set_num_internal_nodes")
+  if(mpl_init_Morphy(nTax, nChar, morphyObj) -> error) {
+    stop("Error ", mpl_translate_error(error), " in mpl_init_Morphy")
+  }
+  if(mpl_attach_rawdata(PhyToString(phy, ';'), morphyObj) -> error) {
+    stop("Error ", mpl_translate_error(error), " in mpl_attach_rawdata")
+  }
+  if(mpl_set_num_internal_nodes(nTax, morphyObj) -> error) { # One is the 'dummy root'
+    stop("Error ", mpl_translate_error(error), " in mpl_set_num_internal_nodes")
+  }
   weight <- attr(phy, 'weight')
   if (any(vapply(seq_len(nChar), function (i) mpl_set_parsim_t(i, 'FITCH', morphyObj), integer(1)) 
-      -> error)) stop("Error ", mpl_translate_error(min(error)), "in mpl_set_parsim_t")
-  if (any(vapply(seq_len(nChar), function (i) mpl_set_charac_weight(i, weight[i], morphyObj),
-      integer(1)) -> error)) stop("Error ", mpl_translate_error(min(error)), "in mpl_set_charac_weight")
-  
+      -> error)) {
+      stop("Error ", mpl_translate_error(min(error)), "in mpl_set_parsim_t")
   }
-
-  if(mpl_apply_tipdata(morphyObj) -> error) stop("Error ", mpl_translate_error(error), " in mpl_apply_tipdata")
+  if (any(vapply(seq_len(nChar), function (i) mpl_set_charac_weight(i, weight[i], morphyObj),
+      integer(1)) -> error)) {
+    stop("Error ", mpl_translate_error(min(error)), "in mpl_set_charac_weight")
+  }
+  if(mpl_apply_tipdata(morphyObj) -> error) {
+    stop("Error ", mpl_translate_error(error), " in mpl_apply_tipdata")
+  }
   return(morphyObj)
 }
 

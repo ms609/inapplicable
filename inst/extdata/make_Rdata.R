8781d5906fac885b92dcbde2cffa5af97ecb2c7d
Martin R. Smith <martins@gmail.com>
2017-05-29 16:00:22 +0100

    Use - as inapplicable token in data sets.  Make generation of datasets more reproducebl with new script.

---
+++ inst/extdata/make_Rdata.R
@@ -0,0 +1,28 @@
+library('phangorn')
+SigSut.data <- read.nexus.data('SigSut-fold.nex')
+contrast <- matrix(c(
+1, 0, 0,
+0, 1, 0,
+0, 0, 1,
+1, 1, 0,
+1, 1, 1), ncol=3, byrow=TRUE, dimnames=list(c(0:1, '-', '+', '?'), c(0, 1, '-')))
+SigSut.phy <- phyDat(SigSut.data, type='USER', contrast=contrast)
+save('SigSut.data', 'SigSut.phy', file='../data/SigSut.RData')
+
+SuttonEtAl.data <- read.nexus.data('SuttonEtAl.nex')
+contrast <- matrix(c(
+1, 0, 0, 0, 0,
+0, 1, 0, 0, 0,
+0, 0, 1, 0, 0,
+0, 0, 0, 1, 0,
+0, 0, 0, 0, 1,
+1, 1, 0, 0, 0, #r
+0, 1, 1, 0, 0, #s
+0, 1, 1, 1, 0, #t
+0, 0, 1, 1, 0, #u
+1, 1, 1, 1, 0, #+
+1, 1, 1, 1, 1), ncol=5, byrow=TRUE, dimnames=list(c(0:3, '-', 'r', 's', 't', 'u','+', '?'), c(0:3, '-')))
+SuttonEtAl.phy <- phyDat(SuttonEtAl.data, type='USER', contrast=contrast)
+save('SuttonEtAl.data', 'SuttonEtAl.phy', file='../data/SuttonEtAl.RData')
+
+  

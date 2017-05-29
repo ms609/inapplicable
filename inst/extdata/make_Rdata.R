7766ad55dbe60844242ade845bc1975a29b0bac8
Martin R. Smith <martins@gmail.com>
2017-05-29 16:00:52 +0100

    Relative filepaths

--- inst/extdata/make_Rdata.R
+++ inst/extdata/make_Rdata.R
@@ -7,7 +7,7 @@ contrast <- matrix(c(
 1, 1, 0,
 1, 1, 1), ncol=3, byrow=TRUE, dimnames=list(c(0:1, '-', '+', '?'), c(0, 1, '-')))
 SigSut.phy <- phyDat(SigSut.data, type='USER', contrast=contrast)
-save('SigSut.data', 'SigSut.phy', file='../data/SigSut.RData')
+save('SigSut.data', 'SigSut.phy', file='../../data/SigSut.RData')
 
 SuttonEtAl.data <- read.nexus.data('SuttonEtAl.nex')
 contrast <- matrix(c(
@@ -23,6 +23,6 @@ contrast <- matrix(c(
 1, 1, 1, 1, 0, #+
 1, 1, 1, 1, 1), ncol=5, byrow=TRUE, dimnames=list(c(0:3, '-', 'r', 's', 't', 'u','+', '?'), c(0:3, '-')))
 SuttonEtAl.phy <- phyDat(SuttonEtAl.data, type='USER', contrast=contrast)
-save('SuttonEtAl.data', 'SuttonEtAl.phy', file='../data/SuttonEtAl.RData')
+save('SuttonEtAl.data', 'SuttonEtAl.phy', file='../../data/SuttonEtAl.RData')
 
   

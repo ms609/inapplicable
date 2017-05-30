8c302ec1a78dc20a9256ae3e9c6dd078e11c3e62
Martin R. Smith <martins@gmail.com>
2017-05-30 16:39:44 +0100

    Document a dataset

---
+++ R/data.R
@@ -0,0 +1,20 @@
+#' Raw data from Zhang et al 2016
+#'
+#' @template LoboMods
+#'
+#' @keywords datasets
+#' @source Zhang X-G, Smith MR, Yang J, Hou J-B. 2016 Onychophoran-like musculature in a phosphatized Cambrian lobopodian. Biol. Lett. 12: 20160492. \url{http://dx.doi.org/10.1098/rsbl.2016.0492}
+"Lobo.data"
+
+#' Data from Zhang et al 2016, in phyDat format
+#'
+#' @template LoboMods
+#'
+#' @format \describe{
+#'    \item{Lobo.data}{a list, each entry reporting the textual representation of the tokens for a named taxon}
+#'    \item{Lobo.phy}{The same data, in \code{phyDat} format.}
+#' }
+#' 
+#' @keywords datasets
+#' @source Zhang X-G, Smith MR, Yang J, Hou J-B. 2016 Onychophoran-like musculature in a phosphatized Cambrian lobopodian. Biol. Lett. 12: 20160492. \url{http://dx.doi.org/10.1098/rsbl.2016.0492}
+"Lobo.phy"

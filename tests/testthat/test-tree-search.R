523ed1c69f1ad9d48b0cb78629127eb32939e9d5
Martin R. Smith <martins@gmail.com>
2017-05-30 08:23:15 +0100

    Re-activate tree search test
    (Now that it passes)

--- tests/testthat/test-tree-search.R
+++ tests/testthat/test-tree-search.R
@@ -1,10 +1,10 @@
 library(ape)
 
 context("tree search")
-#test_that("tree search finds shortest tree", {
+test_that("tree search finds shortest tree", {
   true_tree <- read.tree(text = "(((((1,2),3),4),5),6);")
   dataset <- StringToPhyDat('110000 111000 111100', 1:6, byTaxon=FALSE)
   start_tree <- RenumberTips(read.tree(text = "(((1, 6), 3), (2, (4, 5)));"), true_tree$tip.label)
   expect_equal(InapplicableFitch(start_tree, dataset), 6)
-  expect_equal(InapplicableFitch(TreeSearch(start_tree, dataset), dataset), InapplicableFitch(true_tree, dataset), 3)
+  expect_equal(InapplicableFitch(TreeSearch(start_tree, dataset, maxhits=40, maxiter=10000), dataset), InapplicableFitch(true_tree, dataset), 3)
 })

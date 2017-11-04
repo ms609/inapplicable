ae02024ead0c27726c11611a9b55e529116efcbb
Martin R. Smith <martins@gmail.com>
2017-11-04 14:20:11 +0000

    Remove preamble

--- R/morphyex.R
+++ R/morphyex.R
@@ -1,21 +1,3 @@
-# MDB: This is what a real MorphyLib interface should look like. It is much
-# simpler than what is being attempted. If we want to can some of the
-# more time-intensive tasks inside of some C code, we can do that. But
-# as it stands, the existing C-R interface for Morphy is attempting too
-# many things within the body of one function. Let the user initiate a 
-# Morphy session in R, then add dimensions, raw data, symbols, and any
-# other parameters they wish. They should be able to do all of this in 
-# R through these bindings. If, for instance, we then want to write a 
-# function that calculates the length of a tree, we can do that with R by
-# any number of designs we choose. We could iterate over the tree in R, 
-# but calculating the length by calling only MorphyLib functions. 
-#
-# If we want to can the iteration over the nodes in C, we can write some C
-# to do this, but it doesn't need a giant 5-argument function that tries
-# to do everything in one go. It just needs 2: the tree and the Morphy 
-# object that was initialised in R through the functions below.
-#
-
 #' Converts a numeric error code to human-readable format
 #' 
 #' @param errorCode Non-positive integer to be converted

09d93437f42fc619418cc770832280e372ff884e
Martin R. Smith <martins@gmail.com>
2017-11-09 15:05:42 +0000

    xx

--- R/RandomTreeScore.R
+++ R/RandomTreeScore.R
@@ -1,12 +1,16 @@
 #' Parsimony score of random postorder tree
 #' 
-#' @param nTip number of tips
+#' @param nTip number of tips (minimum 3)
 #' @template morphyObjParam
 #'
 #' @return the parsimony score of a random tree, for the given Morphy object.
 #'
 #' @export
 RandomTreeScore <- function (nTip, morphyObj) {  
+  if (nTip < 3) {
+    warning("nTip < 3 not implemented, as there's only one unrooted topology.")
+    return (0)
+  }
   # Return:
   .Call('BUILD_POSTORDER', as.integer(nTip), morphyObj)
 }
--- inst/tests/benchmark_timing.R
+++ inst/tests/benchmark_timing.R
@@ -1,37 +1,37 @@
 data(inapplicable.datasets)
 scores <- c(
-"Agnarsson2004" =  778 ,
-"Aguado2009" =     579 ,
-"Aria2015" =       143 ,
-"Asher2005" =      345 ,
-"Capa2011" =       385 ,
-"Conrad2008" =     1761,
-"DeAssis2011" =    64  ,
-"Dikow2009" =      1611,
-"Eklund2004" =     440 ,
-"Geisler2001" =    1295,
-"Giles2015" =      710 ,
-"Griswold1999" =   407 ,
-"Liljeblad2008" =  2868,
-"Loconte1991" =    539 ,
-"Longrich2010" =   131 ,
-"OLeary1999" =     508 ,
-"OMeara2014" =     273 ,
-"Rougier2012" =    1215,
-"Rousset2004" =    259 ,
-"Sano2011" =       223 ,
-"Sansom2010" =     189 ,
-"Schulze2007" =    164 ,
-"Shultz2007" =     454 ,
-"Vinther2008" =    79  ,
-"Wetterer2000" =   559 ,
-"Wills2012" =      273 ,
-"Wilson2003" =     879 ,
-"Wortley2006" =    482 ,
-"Zanol2014" =      1311,
-"Zhu2013" =        638 )
+"Agnarsson2004" =  778 , # 0.044 mins             
+"Aguado2009" =     579 , # 4.979             
+"Aria2015" =       143 , # 0.041             
+"Asher2005" =      345 , # 0.00328      
+"Capa2011" =       385 , # 0.131       
+"Conrad2008" =     1761, # 0.119        
+"DeAssis2011" =    64  , # 0            
+"Dikow2009" =      1611, # 22.35        # This is a slow one [22 mins]
+"Eklund2004" =     440 , # 0.080        
+"Geisler2001" =    1295, # 115.3        # This is a slow one [115 mins]
+"Giles2015" =      710 , # 9.80         
+"Griswold1999" =   407 , # 0.020        
+"Liljeblad2008" =  2868, # 1.768        
+"Loconte1991" =    539 , # 0.208        
+"Longrich2010" =   131 , # 0.0033       
+"OLeary1999" =     508 , # 0.158        
+"OMeara2014" =     273 , #              
+"Rougier2012" =    1215, #              
+"Rousset2004" =    259 , #              
+"Sano2011" =       223 , #              
+"Sansom2010" =     189 , #              
+"Schulze2007" =    164 , #              
+"Shultz2007" =     454 , #              
+"Vinther2008" =    79  , #              
+"Wetterer2000" =   559 , #              
+"Wills2012" =      273 , #              
+"Wilson2003" =     879 , #              
+"Wortley2006" =    482 , #              
+"Zanol2014" =      1311, #              
+"Zhu2013" =        638 ) #              
 
-nj.tree <- lapply(inapplicable.phyData, NJTree)
+nj.tree <- lapply(inapplicable.phyData, TreeSearch::NJTree)
 timestart <- double(length(scores))
 timeend   <- double(length(scores))
 names(timestart) <- names(timeend) <- names(scores)
